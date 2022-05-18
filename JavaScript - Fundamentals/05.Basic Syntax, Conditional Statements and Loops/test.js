4dbf33a5fb97cdd788feb995e7377a367ad8171f
angel0w <94199830+angel0w@users.noreply.github.com>
2022-05-18 21:43:23 +0300

    add file

---
+++ JavaScript - Fundamentals/05.Basic Syntax, Conditional Statements and Loops/test.js
@@ -0,0 +1,17 @@
+// function solve (x) {
+//     console.log(x.toFixed(2));
+// }
+// solve(5.256)
+
+
+//-----1 
+
+// function multiplyNumberBy2(input) {
+//  console.log(5 === 4)
+// }
+
+// multiplyNumberBy2()
+
+
+//----2
+
