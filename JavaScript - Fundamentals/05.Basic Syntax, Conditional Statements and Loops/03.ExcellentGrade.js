569d3c0ca7fc30ca533fb744b3434467befd32f0
angel0w <94199830+angel0w@users.noreply.github.com>
2022-05-19 10:55:13 +0300

    solving problems

---
+++ JavaScript - Fundamentals/05.Basic Syntax, Conditional Statements and Loops/03.ExcellentGrade.js
@@ -0,0 +1,9 @@
+function excellentGrade(num) {
+    if (num >= 5.50) {
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+excellentGrade(4.35)
+excellentGrade(5.50)
