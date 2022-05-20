e699af18d7011f73f80a8926038512a268a8c5a9
angel0w <94199830+angel0w@users.noreply.github.com>
2022-05-20 15:16:10 +0300

    solv 02

--- JavaScript - Fundamentals/06.Exercise Basic Syntax/02. Rounding.js
+++ JavaScript - Fundamentals/06.Exercise Basic Syntax/02. Rounding.js
@@ -1,10 +1,11 @@
 function rounding (rounded, precision) {
-    for (let i = 0; i <= rounded; i++) {
-        if ( rounded.length > 15) {
-            console.log(rounded.toFixed(15))
+
+        if ( precision <= 15) {
+            console.log(parseFloat(rounded.toFixed(15)));
+        } else {
+            console.log(parseFloat(rounded.toFixed(precision)))
         }
-        console.log(rounded.toFixed(precision))
-    }
 }
+rounding(10.5,3)
 rounding(3.1415926535897932384626433832795,2)
-// rounding(10.5,3)
+rounding(3.1415926535897932384626433832795,16)
