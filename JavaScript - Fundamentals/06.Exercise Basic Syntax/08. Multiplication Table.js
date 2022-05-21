39fca5fca928a5c5d0361444e9c146b32c65fda2
angel0w <94199830+angel0w@users.noreply.github.com>
2022-05-21 15:24:18 +0300

    solve problems

--- JavaScript - Fundamentals/06.Exercise Basic Syntax/08. Multiplication Table.js
+++ JavaScript - Fundamentals/06.Exercise Basic Syntax/08. Multiplication Table.js
@@ -1,6 +1,7 @@
-// function multiplicationTable (num) {
-//     for (let i = 1; i <= 10; i++) {
-//         console.log(`${num} X ${i} = ${i* num}`)
-//     }
-// }
-// multiplicationTable(5)
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+multiplicationTable(5);
+multiplicationTable(2);
