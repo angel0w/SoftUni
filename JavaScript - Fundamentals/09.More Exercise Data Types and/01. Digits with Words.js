27a15fc8638ac6334a5bb3c51ba49e2d72e9a77c
angel0w <94199830+angel0w@users.noreply.github.com>
2022-05-28 00:04:29 +0300

    add more problems

---
+++ JavaScript - Fundamentals/09.More Exercise Data Types and/01. Digits with Words.js
@@ -0,0 +1,26 @@
+function digitWithWords(num) {
+  if (num === "nine") {
+    console.log(9);
+  } else if (num === "eight") {
+    console.log(8);
+  } else if (num === "seven") {
+    console.log(7);
+  } else if (num === "six") {
+    console.log(6);
+  } else if (num === "five") {
+    console.log(5);
+  } else if (num === "four") {
+    console.log(4);
+  } else if (num === "three") {
+    console.log(3);
+  } else if (num === "two") {
+    console.log(2);
+  } else if (num === "one") {
+    console.log(1);
+  } else if (num === "zero") {
+    console.log(0);
+  }
+}
+// digitWithWords("nine");
+digitWithWords("two");
+// digitWithWords("zero");
---
+++ JavaScript - Fundamentals/09.More Exercise Data Types and/02. Prime Number Checker.js
@@ -0,0 +1,4 @@
+function primeNumberChecker (number) {
+     
+}
+primeNumberChecker();
