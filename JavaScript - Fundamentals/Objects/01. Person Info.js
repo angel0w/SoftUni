be37b6865ce002498fdf8a2d3a25a65fa67c8784
angel0w <94199830+angel0w@users.noreply.github.com>
2022-07-02 10:26:43 +0300

    objs

---
+++ JavaScript - Fundamentals/Objects/01. Person Info.js
@@ -0,0 +1,9 @@
+function personInfo(firstName,lastName,age) {
+
+    let res = {}
+    res.firstName = firstName;
+    res.lastName = lastName;
+    res.age = age
+    return res
+}
+console.log(personInfo('Peter','Pan','20')); 
---
+++ JavaScript - Fundamentals/Objects/test.js
@@ -0,0 +1,14 @@
+function app () {
+    let myObj = {
+        name: 'Todor',
+        age: 28,
+        sayHello: function () {
+            console.log("Hello");
+        }
+    }
+
+    console.table(myObj);
+    console.log(myObj.name, myObj.age);
+    console.log(myObj.);
+}
+app();
