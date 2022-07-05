5698b6c585db7acd2da273866e100d42f02fc499
angel0w <94199830+angel0w@users.noreply.github.com>
2022-07-05 16:17:31 +0300

    convert to Object

---
+++ JavaScript - Fundamentals/Objects/03. Convert to Object.js
@@ -0,0 +1,10 @@
+function convertToObject (input) {
+
+    
+    let person = JSON.parse(input);
+
+    for(let key of Object.keys(person)) {
+        console.log(`${key}: ${person[key]}`);
+    }
+}
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
