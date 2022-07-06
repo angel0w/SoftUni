882643702ca8c77a19caad8e8bf0084640113069
angel0w <94199830+angel0w@users.noreply.github.com>
2022-07-06 18:12:31 +0300

    solution

--- JavaScript - Fundamentals/Objects/05. Cats.js
+++ JavaScript - Fundamentals/Objects/05. Cats.js
@@ -1,31 +1,57 @@
 function catFactory (catsData) {
+
+    let cats = [];
+
     class Cat {
-        constructor(name, age){
+        constructor (name, age){
             this.name = name;
             this.age = age;
-
         }
-        meow(){
+        meow() {
             console.log(`${this.name}, age ${this.age} says Meow`);
         }
     }
 
-    let cats = []
+    for(let cat of catsData ){
+        let tokens = cat.split(' ');
+        let catName = tokens[0];
+        let catAge = tokens[1];
 
-    for (let catDate of catsData){
-        console.log(catDate);
-        let token = catDate.split(' ');
-        console.log(token);
-        let catName = token[0];
-        let catAge = token[1];
-        let myCat = new Cat(catName, catAge);
+        let myCat = new Cat (catName, catAge);
         cats.push(myCat);
+
     }
 
-    for (let myCat of cats){
-        myCat.meow();
+    for(let cat of cats){
+        cat.meow();
     }
-
 }
 catFactory(['Mellow 2', 'Tom 5']);
 catFactory(['Candy 1', 'Poppy 3', 'Nyx 2']);
+
+
+
+// class Cat {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+
+//     }
+//     meow(){
+//         console.log(`${this.name}, age ${this.age} says Meow`);
+//     }
+// }
+
+// let cats = []
+
+// for (let catDate of catsData){
+//     let token = catDate.split(' ');
+//     let catName = token[0];
+//     let catAge = token[1];
+//     let myCat = new Cat(catName, catAge);
+//     cats.push(myCat);
+// }
+
+// for (let myCat of cats){
+//     myCat.meow();
+// }
