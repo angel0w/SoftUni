f5944b825e2633357fea8521f070f97a64ebf830
angel0w <94199830+angel0w@users.noreply.github.com>
2022-09-24 22:24:22 +0300

    .

--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/03. Accordion/accordion.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,25 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.querySelector('.button');
+    let extra = document.getElementById('extra');
+
+    btn.textContent =
+        btn.textContent == 'More'
+            ? (btn.textContent = 'Less')
+            : (btn.textContent = 'More');
+    // if (btn.textContent === 'More') {
+    //     btn.textContent = 'Less';
+    // } else {
+    //     btn.textContent = 'More';
+    // }
+
+    extra.style.display =
+        extra.style.display == 'none' || extra.style.display == ' '
+            ? (extra.style.display = 'block')
+            : (extra.style.display = 'none');
+
+    // if (extra.style.display == 'none' || extra.style.display == ' ') {
+    //     extra = extra.style.display = 'block';
+    // } else {
+    //     extra = extra.style.display = 'none';
+    // }
 }
