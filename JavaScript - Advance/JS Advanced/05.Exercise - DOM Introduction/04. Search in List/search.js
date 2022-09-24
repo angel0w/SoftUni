8b24d501d162116e5101a29d81e9a9e5e234e4ac
angel0w <94199830+angel0w@users.noreply.github.com>
2022-09-24 23:24:49 +0300

    .

--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/04. Search in List/search.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/04. Search in List/search.js
@@ -1,19 +1,28 @@
 function search() {
-   // TODO
-   let townList = Array.from(document.querySelectorAll('ul li'));
-   let searchText = document.getElementById('searchText').value;
-   let count = 0;
-   for (let item of townList) {
-      let text = item.textContent;
+    // TODO
+    let towns = Array.from(document.querySelectorAll('ul li'));
+    let searchText = document.getElementById('searchText').value;
+    let counter = 0;
 
-      if (text.includes(searchText)) {
-         item.style.textDecoration = 'underline';
-         item.style.fontWeight = 'bold';
-         count++
-      } else {
-         item.style.textDecoration = 'none';
-         item.style.fontWeight = '';
-      }
+    if (searchText.length === 0) {
+        return (document.getElementById(
+            'result'
+        ).innerText = `0 matches found`);
+    }
+
+    for (let town of towns) {
+        let text = town.textContent;
+
+        if (text.includes(searchText)) {
+            town.style.textDecoration = 'underline';
+            town.style.fontWeight = 'bold';
+            counter++;
+        } else {
+            town.style.textDecoration = 'none';
+            town.style.fontWeight = '';
+        }
+    }
+
+    document.getElementById('result').innerText = `${counter} matches found`;
+    debugger
    }
-   document.getElementById('result').innerText = `${count} matches found`;
-}
