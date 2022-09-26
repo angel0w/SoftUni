9774168908174631e0445a0fbc45be9ae57419f8
angel0w <94199830+angel0w@users.noreply.github.com>
2022-09-26 16:19:20 +0300

    .

--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/06. Format the Text/solution.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+    //TODO
+
+    let text = document.getElementById('input').value;
+    let splittedText = text.split('.').filter((el) => el != '');
+
+    let output = document.getElementById('output');
+
+    for (let i = 0; i < splittedText.length; i += 3) {
+        let arr = [];
+        for (let j = 0; j < 3; j++) {
+            if (splittedText[i + j]) {
+                arr.push(splittedText[i + j]);
+            }
+        }
+        let paragraph = arr.join('. ') + '.';
+        output.innerHTML += `<p>${paragraph}</p>`;
+    }
 }
