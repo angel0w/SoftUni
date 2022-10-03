f4eebbfc449e701fb0da705a61f79bbc175e623d
angel0w <94199830+angel0w@users.noreply.github.com>
2022-10-03 22:44:13 +0300

    .

--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/04. Fill-Dropdown/dropdown.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+    let newItem = document.getElementById('menu');
+
+    let option = document.createElement('option');
+
+    option.textContent = text.value;
+    option.value = value.value;
+
+    newItem.appendChild(option);
+
+    text.value = '';
+    value.value = '';
 }
