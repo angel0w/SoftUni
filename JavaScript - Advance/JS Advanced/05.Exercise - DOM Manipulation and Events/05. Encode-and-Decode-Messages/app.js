fb07c390156b7c7a7bd9dcbc7d51cb721f13ccaf
angel0w <94199830+angel0w@users.noreply.github.com>
2022-10-03 23:41:29 +0300

    .

--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textarea = document.querySelectorAll('textarea');
+    let btn = document.querySelectorAll('button');
+
+    btn[0].addEventListener('click', encode);
+    btn[1].addEventListener('click', decode);
+
+    function encode (e) {
+        let encodeMessage = '';
+        let input = textarea[0].value;
+
+        for (let i = 0;  i < input.length; i++) {
+            encodeMessage += String.fromCharCode(input[i].charCodeAt(0) + 1);
+        }
+        
+        textarea[1].value = encodeMessage;
+        textarea[0].value = ''
+    }
+
+    function decode(e) {
+        let decodeMessage = '';
+        let input = textarea[1].value;
+
+        for (let i = 0;  i < input.length; i++) {
+            decodeMessage += String.fromCharCode(input[i].charCodeAt(0) - 1);
+        }
+
+        textarea[1].value = decodeMessage;
+    }
 }
--- JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
+++ JavaScript - Advance/JS Advanced/05.Exercise - DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,28 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let btnConvert = document.getElementById('convert');
+    btnConvert.addEventListener('click', onClick);
+
+    let metrickUnits = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    function onClick(e) {
+        let fromValue = document.getElementById('inputUnits').value;
+        let toValue = document.getElementById('outputUnits').value;
+
+        let inputDistance = Number(document.getElementById('inputDistance').value);
+        let ouputDistanceEl = document.getElementById('outputDistance');
+
+        let valueInMeters = inputDistance * metrickUnits[fromValue];
+        let convertedValue = valueInMeters / metrickUnits[toValue];
+
+        ouputDistanceEl.value = convertedValue;
+    }
 }
