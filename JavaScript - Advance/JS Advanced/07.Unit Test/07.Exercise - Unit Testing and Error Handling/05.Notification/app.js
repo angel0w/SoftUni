7b853336de166003b28b1dbf5f9df6d8226ae861
angel0w <94199830+angel0w@users.noreply.github.com>
2022-10-10 00:28:22 +0300

    .

--- JavaScript - Advance/JS Advanced/07.Unit Test/07.Exercise - Unit Testing and Error Handling/05.Notification/app.js
+++ JavaScript - Advance/JS Advanced/07.Unit Test/07.Exercise - Unit Testing and Error Handling/05.Notification/app.js
@@ -1,13 +1,12 @@
 function notify(message) {
-  console.log('todo')
-  let divContent = document.getElementById('notification');
-  divContent.innerHTML = message;
-  
-  divContent.style.display = "block";
-  
-  divContent.addEventListener('click', toogleDispayStyle);
-  
-  function toogleDispayStyle (e) {
-    e.target.style.display = 'none';
+  let divContainer = document.getElementById('notification');
+  divContainer.textContent = message;
+  divContainer.style.display = 'block'
+
+  divContainer.addEventListener('click', onClick);
+
+
+  function onClick(e) {
+    e.target.style.display = 'none'
   }
 }
