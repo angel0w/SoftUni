8942e04aef648590f874b6af13bb37c68c5d0fe8
angel0w <94199830+angel0w@users.noreply.github.com>
2023-09-12 22:13:42 +0300

    +

--- JavaScript - Advance/reviewing issues again/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ JavaScript - Advance/reviewing issues again/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+//     Write a program that takes an email from an input field and deletes the matching row from a table.
+    let emails = Array.from(document.querySelectorAll('tbody tr td:nth-child(even)'));
+    let msgResult = document.getElementById('result');
+    let input = document.querySelector('input[name="email"]').value;
+    
+    let flag = 'false'
+
+    for (const email of emails) {
+        if (email.textContent == input) {
+            email.parentElement.remove()
+            flag = 'true';
+            msgResult.textContent = "Deleted."
+        } else {
+            msgResult.textContent = "Not found."
+
+        }
+        
+    }
+    document.querySelector('input[name="email"]').value = ''
+    
 }
--- JavaScript - Advance/reviewing issues again/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ JavaScript - Advance/reviewing issues again/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
     //TODO...
+    let ulItems = document.getElementById('items');
+    let inputValue = document.getElementById('newItemText').value;
+
+    let li = document.createElement('li');
+    li.textContent = inputValue;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = 'Delete';
+
+    
+    deleteBtn.addEventListener('click', function(ev) {
+        // console.log(ev.target.parentElement.delete);
+        ev.target.parentElement.remove();
+    });
+    
+
+    if(inputValue !== '') {
+        li.append(deleteBtn);
+        ulItems.appendChild(li);
+
+    }
+    document.getElementById('newItemText').value = '';
+
+
+
 }
